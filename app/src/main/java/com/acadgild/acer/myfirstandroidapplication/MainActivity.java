f8d90e16fcfb35861a066a49541614baed61b2ba
JyotiSai <kajal.jojo@gmail.com>
2018-01-22 09:32:45 +0530

    There are errors

---
+++ app/src/main/java/com/acadgild/acer/myfirstandroidapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package com.acadgild.acer.myfirstandroidapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //this method is used to create an activity
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
